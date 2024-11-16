2869b6fb46e68184cc0ce5143edd67c3d7624639
Kavindu Perera <59706267+kavindu-pere@users.noreply.github.com>
2024-11-16 22:28:18 +0530

    Initial commit

---
+++ src/main/java/dev/kavindu/qlistener/QListenerApplication.java
@@ -0,0 +1,13 @@
+package dev.kavindu.qlistener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QlistenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QlistenerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/kavindu/qlistener/QListenerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.kavindu.qlistener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QListenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
